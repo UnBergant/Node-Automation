bf853ed53cce00acd74f1b264ede74c7826afcfe
Igor Druzhinin <152170719+UnBergant@users.noreply.github.com>
2025-03-24 14:56:18 +0100

    Update swap.js

--- t3rn/swap.js
+++ t3rn/swap.js
@@ -1,11 +1,53 @@
 const amountInputSelector = "#root-scroll-container > div > div > div.relative.flex.justify-center > div.border.border-white\\/15.bg-card-2.text-card-foreground.shadow-sm.p-4.md\\:p-6.rounded-\\[1\\.5rem\\].w-full.sm\\:w-\\[484px\\].md\\:w-\\[460px\\].lg\\:w-\\[532px\\] > div > section > div.pt-6.flex.flex-col.gap-3 > form > div > div.flex.flex-col.gap-10.md\\:gap-4.relative > div:nth-child(1) > div > div.bg-white\\/5.p-4.rounded-\\[24px\\].hover\\:bg-white\\/10.transition-colors.w-auto.flex.flex-col.h-\\[117px\\].px-4.relative > input";
 const swapButtonSelector = "#root-scroll-container > div > div > div.relative.flex.justify-center > div.border.border-white\\/15.bg-card-2.text-card-foreground.shadow-sm.p-4.md\\:p-6.rounded-\\[1\\.5rem\\].w-full.sm\\:w-\\[484px\\].md\\:w-\\[460px\\].lg\\:w-\\[532px\\] > div > section > div.pt-6.flex.flex-col.gap-3 > form > button";
 const newOrderButtonSelector = "#root-scroll-container > div > div > div.relative.flex.justify-center > div.border.border-white\\/15.bg-card-2.text-card-foreground.shadow-sm.p-4.md\\:p-6.rounded-\\[1\\.5rem\\].w-full.sm\\:w-\\[484px\\].md\\:w-\\[460px\\].lg\\:w-\\[532px\\] > div > section > div.pt-4 > a";
+const goBackButtonSelector = "#root-scroll-container > div > div > div.relative.flex.justify-center > div.border.border-white\\/15.bg-card-2.text-card-foreground.shadow-sm.p-4.md\\:p-6.rounded-\\[1\\.5rem\\].w-full.sm\\:w-\\[484px\\].md\\:w-\\[460px\\].lg\\:w-\\[532px\\] > div > section > div.pt-4 > div > button"
 
-const AMOUNT = 0.5;
+const AMOUNT = 1;
 const TIMEOUT_INTERVAL = 1000;
 
-const CONFIRM_TRANSACTION_TEXT = 'Confirm transaction';
+const clickEl = ({selector, text, desc}) => {
+    const el = document.querySelector(selector);
+    console.log(`Press: ${text}\nDesc: ${desc}`);
+    el.click();
+}
+
+const TEXTS = {
+    swapButton: 'Confirm transaction',
+    goBackButton: 'Go Back'
+}
+
+const DESCS = {
+    amountInput: 'Token amount input',
+    swapButton: 'Confirm swap button',
+    newOrderButton: 'Go back to create new order',
+    goBackButton: 'In case of error this button is shown'
+}
+
+const amountInputConf = {
+    selector: amountInputSelector,
+    desc: DESCS.amountInput
+}
+
+const swapButtonConf = {
+    selector: swapButtonSelector, 
+    text: TEXTS.swapButton,
+    desc: DESCS.swapButton
+}
+
+const newOrderButtonConf = {
+    selector: newOrderButtonSelector,
+    action: clickEl,
+    desc: DESCS.newOrderButton,
+}
+
+const goBackButtonConf = {
+    selector: goBackButtonSelector, 
+    action: clickEl,
+    text: TEXTS.goBackButton,
+    desc: DESCS.goBackButton,
+}
+
 
 const promisify = (f, arg) => {
     return new Promise((res, rej) => {
@@ -35,25 +77,52 @@ const makeSwap = () => {
     swapButton.click();
 }
 
-const isElementVisible = (elSelector) => {
-    const el = document.querySelector(elSelector);
+const isElementVisible = ({selector, desc}) => {
+    const el = document.querySelector(selector);
     if (!el) return false;
-    console.log('element found');
+    console.log(`Element is visible: \n${desc}`);
     const rect = el.getBoundingClientRect();
     return rect.top >= 0 && rect.left >= 0 &&
            rect.bottom <= window.innerHeight &&
            rect.right <= window.innerWidth;
 }
 
-const textOfELementIsEqual = ({elSelector, text}) => {
-    const el = document.querySelector(elSelector);
-    console.log({elSelector, text});
-    if (!el) return false;
-    console.log({text});
-    console.log({el: el.innerText});
-    if (el.disabled) return false; 
-    if (el.innerText === text) return true;
+const textOfELementIsEqual = ({selector, text}) => {
+    const el = document.querySelector(selector);
+    console.log(`Looking for element with text "${text}"...`);
+    if (!el) {
+        console.log(`Element with text "${text}" not found`);
+        return false;
+    }
+    if (el.disabled)  {
+        console.log(`Element with text "${text}" is disabled`);
+        return false
+    }; 
+    if (el.innerText === text) {
+        console.log(`Element with text "${text}" found`);
+        return true;
+    }
 } 
+
+const doActionWithAny = (els) => {
+    for (let el of els) {
+        if (!isElementVisible(el)) {
+            continue;
+        };
+        switch (true) {
+            case !isElementVisible(el):
+                continue;
+            case el.text && textOfELementIsEqual(el):
+                el.action(el);
+                return el;
+            case !el.text:
+                el.action(el);
+                return el;
+            default:
+                console.log('default block')
+        }
+    }
+};
 
 const clickNewOrderButton = () => {
     const newOrderButton = document.querySelector(newOrderButtonSelector);
@@ -61,16 +130,11 @@ const clickNewOrderButton = () => {
 }
 
 const main = async () => {
-    const swapButtonConf = {
-        elSelector: swapButtonSelector, 
-        text: CONFIRM_TRANSACTION_TEXT
-    }
-    await promisify(isElementVisible, amountInputSelector);
+    await promisify(isElementVisible, amountInputConf);
     setAmount(AMOUNT);
     await promisify(textOfELementIsEqual, swapButtonConf);
     makeSwap();
-    await promisify(isElementVisible, newOrderButtonSelector);
-    clickNewOrderButton();
+    await promisify(doActionWithAny, [goBackButtonConf, newOrderButtonConf])
     main();
 }
 
